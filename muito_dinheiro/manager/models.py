0d1f7dc4af2e48cf5a65902cc17372aa512db5b1
rcd1337 <56580332+rcd1337@users.noreply.github.com>
2021-05-17 07:03:10 -0300

    created models

--- muito_dinheiro/manager/models.py
+++ muito_dinheiro/manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"(id: {self.id}) (name: {self.name}) (created_on: {self.created_on}"
+
+class Operation(models.Model):
+    client = models.ForeignKey("Client", on_delete=models.CASCADE, related_name="operations")
+    source_currency = models.CharField(max_length=255, blank=False, null=False)
+    target_currency = models.CharField(max_length=255, blank=False, null=False)
+    source_amount = models.DecimalField(max_digits=15, decimal_places=2, blank=False)
+    converted_amount = models.DecimalField(max_digits=15, decimal_places=2, blank=False)
+    fee = models.DecimalField(max_digits=14, decimal_places=2, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"""(id: {self.id}) (client: {self.client.name})
+        (source: {self.source_currency}) (target: {self.target_currency}) (amount: {self.source_currency})
+        (converted: {self.converted_amount}) (fee: {self.fee}) (created on: {self.created_on})"""
