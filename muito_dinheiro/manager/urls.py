0f734f3586f5b4940336ba1dafe367d3c87c2db0
rcd1337 <56580332+rcd1337@users.noreply.github.com>
2021-05-18 04:30:51 -0300

    created paths

--- muito_dinheiro/manager/urls.py
+++ muito_dinheiro/manager/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cadastrar_cliente", views.add_client, name="add_client"),
+    path("cadastrar_operacao", views.add_operation, name="add_operation"),
+    path("relatorio", views.report, name="report"),
+    path("relatorios", views.reports, name="reports"),
 ]
