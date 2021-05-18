3bc902e64b8edaffd6cd70f7340e7b52abf922dd
rcd1337 <56580332+rcd1337@users.noreply.github.com>
2021-05-18 04:31:08 -0300

    created base views

--- muito_dinheiro/manager/views.py
+++ muito_dinheiro/manager/views.py
@@ -1,5 +1,27 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+import datetime
+
+from .models. import Client, Operation
 
 # Create your views here.
-def index():
+def index(request):
+    return render(request, "manager/index.html")
+
+
+def add_client(resquest):
+    pass
+
+
+def add_operation(request):
+    pass
+
+
+def report(request):
+    pass
+
+
+def reports(request):
     pass
